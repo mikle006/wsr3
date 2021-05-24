befe08c53bc1cdfaf2e7e759d72eb341ef8862e5
mikle006 <kurch7815@gmail.com>
2021-05-24 22:51:32 +0300

    update database

--- !DATABASE/wsr.sql
+++ !DATABASE/wsr.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 24 2021 г., 14:56
--- Версия сервера: 5.7.23
--- Версия PHP: 7.2.10
+-- Время создания: Май 24 2021 г., 22:50
+-- Версия сервера: 10.3.13-MariaDB
+-- Версия PHP: 7.1.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- База данных: `wsr`
 --
+CREATE DATABASE IF NOT EXISTS `wsr` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `wsr`;
 
 -- --------------------------------------------------------
 
@@ -52,7 +54,7 @@ CREATE TABLE `request` (
   `name` varchar(255) NOT NULL COMMENT 'Название ',
   `before_img` varchar(255) NOT NULL COMMENT 'Фото до',
   `after_img` varchar(255) DEFAULT NULL COMMENT 'Фото после ',
-  `why_not` text COMMENT 'Причина отказа',
+  `why_not` text DEFAULT NULL COMMENT 'Причина отказа',
   `category_id` int(11) NOT NULL COMMENT 'Категория',
   `created_at` datetime NOT NULL,
   `created_by` int(11) NOT NULL COMMENT 'Автор',
@@ -64,13 +66,9 @@ CREATE TABLE `request` (
 --
 
 INSERT INTO `request` (`id`, `status`, `name`, `before_img`, `after_img`, `why_not`, `category_id`, `created_at`, `created_by`, `updated_by`) VALUES
-(1, '', 'ffffffffffff', '1', '', '', 1, '2021-05-15 17:34:00', 100, 100),
 (2, 'Новая', 'fff', 'uploads/gory_reka_ozero_trava_leto_99409_1920x1080.jpg', '', '', 1, '2021-05-15 20:23:20', 100, 100),
-(3, 'Решена', 'ааааа', 'uploads/Без названия.png', '', '', 1, '2021-05-15 21:51:44', 100, 100),
-(4, 'Решена', 'ffffffff', 'uploads/gory_reka_ozero_trava_leto_99409_1920x1080.jpg', '', '', 1, '2021-05-23 16:00:53', 100, 100),
 (5, 'Новая', 'ffffffff13213', 'uploads/autumn-forest-road-scenery.jpg', '', '', 1, '2021-05-23 16:05:10', 100, 100),
-(6, 'Новая', 'rE11', 'uploads/autumn-forest-road-scenery.jpg', '', '', 1, '2021-05-23 16:46:11', 101, 101),
-(7, 'Решена', 'ffffffff312312311312', 'uploads/gory_reka_ozero_trava_leto_99409_1920x1080.jpg', '', 'FSDFSSFDSD', 1, '2021-05-23 17:20:49', 100, 100);
+(8, 'Решена', 'pepachka', 'uploads/ygeFYHcYHt4.jpg', '', 'ыф', 1, '2021-05-24 21:05:34', 100, 100);
 
 --
 -- Индексы сохранённых таблиц
@@ -103,7 +101,7 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT для таблицы `request`
 --
 ALTER TABLE `request`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
